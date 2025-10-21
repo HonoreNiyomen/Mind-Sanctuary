1a0b601c09ba5179755fa571233b01f7b7d733e4
Abdurrahmaan9 <ac104442@students.cavendish.co.zm>
2025-10-21 15:23:11 +0200

    pulling

--- config/test.exs
+++ config/test.exs
@@ -9,8 +9,8 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :mind_sanctuary, MindSanctuary.Repo,
-  username: "root",
-  password: "Qwerty12",
+  username: "postgres",
+  password: "postgres",
   hostname: "localhost",
   database: "mind_sanctuary_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,

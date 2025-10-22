0de0f98c33753eebc2bf1cddd84b1f83d8af8573
Abdurrahmaan9 <ac104442@students.cavendish.co.zm>
2025-10-22 17:01:49 +0200

    enhanced the the nav bar for easy navigation

--- lib/mind_sanctuary_web/live/dashboard_live/index.ex
+++ lib/mind_sanctuary_web/live/dashboard_live/index.ex
@@ -1,7 +1,6 @@
 defmodule MindSanctuaryWeb.DashboardLive.Index do
   use MindSanctuaryWeb, :live_view
 
-  alias MindSanctuary.Accounts.Scope
 
   @impl true
   def mount(_params, _session, socket) do
